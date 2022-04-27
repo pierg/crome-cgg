92175727ee93c0e8cd2a7b6840ba761e795a1417
Piergiuseppe Mallozzi <piergiuseppe.mallozzi@gmail.com>
2022-04-26 20:17:10 -0700

    adding persistance

--- examples/persistence.py
+++ examples/persistence.py
@@ -1,4 +1,5 @@
-from crome_cgg.tools.persistence import dump_world, load_world, dump_goals, load_goals
+from crome_cgg.cgg import Cgg
+from crome_cgg.tools.persistence import dump_world, load_world, dump_goals, load_goals, dump_cgg, load_cgg
 from crome_contracts.contract import Contract
 from crome_logic.patterns.robotic_movement import StrictOrderedPatrolling
 from crome_logic.specification.temporal import LTL
@@ -23,8 +24,12 @@ w = World(
             BooleanLocation(
                 name="r1", mutex_group="locations", adjacency_set={"r2", "r5"}
             ),
+            BooleanLocation(
+                name="r3", mutex_group="locations", adjacency_set={"r2", "r5"}
+            ),
             BooleanSensor(name="person"),
-            BooleanContext(name="day", mutex_group="time")
+            BooleanContext(name="day", mutex_group="time"),
+            BooleanContext(name="night", mutex_group="time")
         }
     ),
 )
@@ -44,9 +49,30 @@ goals = {Goal(
             typeset=w.typeset,
         )
     ),
-    world=w
-)}
+    world=w),
+    Goal(
+        id="night_patrol_34",
+        description="During context night => start from r3, patrol r3, r4 in strict order,\n"
+                    "Strict Ordered Patrolling Location r3, r4",
+        context=w["night"],
+        contract=Contract(
+            LTL(
+                StrictOrderedPatrolling(locations=["r3"]).__str__(),
+                typeset=w.typeset,
+            )
+        ),
+        world=w,
+    )
+}
 
 dump_goals(goals, folder_name=project_name)
 goals_retrieved = load_goals(folder_name=project_name)
 assert goals == goals_retrieved
+
+
+cgg = Cgg(init_goals=goals)
+
+dump_cgg(cgg, folder_name=project_name)
+cgg_retrieved = load_cgg(folder_name=project_name)
+# TODO: [PIER] Check why objects are slightly different
+assert cgg == cgg_retrieved
