7de20bdee4eb0cd0f08a12de3886f385b6597063
Piergiuseppe Mallozzi <piergiuseppe.mallozzi@gmail.com>
2022-05-12 17:14:18 -0700

    work in progress

--- case_studies/repair/cgg.py
+++ case_studies/repair/cgg.py
@@ -1,9 +1,61 @@
 from case_studies.repair.goals import goals
 from case_studies.repair.world import world
-from crome_cgg.cgg import Cgg
+from crome_cgg.cgg import Cgg, Link
+from crome_cgg.goal import Goal
+from crome_cgg.library import Library
+from crome_contracts.contract import Contract
+from crome_logic.patterns.robotic_movement import Patrolling
+from crome_logic.specification.temporal import LTL
 
 w = world
 
 cgg = Cgg(goals)
 
 print(cgg)
+
+l1 = Goal(
+    id="patrol_l1_l2",
+    description="Keep visiting l1 and l2",
+    contract=Contract(LTL(Patrolling(["l1", "l2"]), w.typeset)),
+    world=w,
+)
+
+l2 = Goal(
+    id="patrol_l1_l2_l3_l4",
+    description="Keep visiting l1 and l2",
+    contract=Contract(LTL(Patrolling(["l1", "l2", "l3", "l4"]), w.typeset)),
+    world=w,
+)
+
+
+library = Library({l1, l2})
+
+leaves = cgg.leaves
+
+candidate_goals = set()
+for goal in leaves:
+    if library.covers(goal):
+        candidate_goals.add(goal)
+print(
+    f"Candidate goals which are covered by the library:\n{[g.id for g in list(candidate_goals)]}"
+)
+
+
+refined_goals = set()
+
+for goal in leaves:
+    refinement = library.search_refinement(goal)
+    if refinement is not None:
+        print(f"{refinement.id}\trefines\t{goal.id}")
+        """We attach the refinement to the existing goal"""
+        cgg.add_edge(goal, refinement, Link.refinement)
+        refined_goals.add(goal)
+
+print(
+    f"There are still {len(candidate_goals - refined_goals)} goals that are covered by the library "
+    f"but are have no refinement"
+)
+
+
+
+
---
+++ crome_cgg/library/__init__.py
@@ -0,0 +1,139 @@
+from __future__ import annotations
+
+import itertools
+from collections import Counter
+from dataclasses import dataclass
+from functools import reduce
+
+from crome_cgg.goal import Goal
+from crome_cgg.goal.operations.composition import g_composition
+from crome_logic.typeset import Typeset
+
+
+@dataclass
+class Library:
+
+    def __init__(self, goals: set[Goal] | None):
+        if goals is None:
+            self._goals = set()
+        else:
+            self._goals: set[Goal] = goals
+
+        self._typeset = Typeset()
+
+        for goal in self.goals:
+            self._typeset += goal.contract.typeset
+
+    @property
+    def goals(self) -> set[Goal]:
+        return self._goals
+
+    @property
+    def typeset(self) -> Typeset:
+        return self._typeset
+
+    def add_goals(self, goals: set[Goal]):
+        self._goals |= goals
+        for goal in goals:
+            self._typeset |= goal.contract.typeset
+
+    def get_candidate_composition(self, goal_to_refine: Goal):
+
+        candidates = Counter()
+        best_similar_types = 0
+
+        for n in range(1, len(self.goals)):
+            for subset in itertools.combinations(self.goals, n):
+                n_compositions = len(subset)
+                if n_compositions == 1:
+                    subset_typeset = subset[0].specification.typeset
+                else:
+                    subset_typeset = reduce(
+                        (lambda x, y: x | y), [g.specification.typeset for g in subset]
+                    )
+                similar_types = self.covers(goal_to_refine, subset_typeset)[1]
+                if similar_types > best_similar_types:
+                    candidates = Counter()
+                    best_similar_types = similar_types
+                elif similar_types == best_similar_types:
+                    candidates[g_composition(subset)] = n_compositions
+
+        print(
+            f"There are {len(candidates.keys())} candidates with the same number of similar types: {best_similar_types}"
+        )
+
+        for node, count in candidates.items():
+            print(f"{node.name}: {count}")
+
+        # Filtering candidates with too many goals composed
+        new_candidates = [
+            x for x, count in candidates.items() if count == min(candidates.values())
+        ]
+
+        print("filtering...")
+        print("\n".join(e.name for e in new_candidates))
+
+        winner = self.get_most_refined(new_candidates)
+        print(f"{winner.id} is the most refined candidate")
+        return winner
+
+    def get_most_refined(self, goals: list[Goal]) -> Goal:
+
+        scores = Counter()
+
+        for a, b in itertools.permutations(goals, 2):
+            if a <= b:
+                scores[a] += 1
+
+        print("ratings")
+        for node, count in scores.items():
+            print(f"{node.name}: {count}")
+
+        return scores.most_common(1)[0][0]
+
+    def search_refinement(self, goal_to_refine: Goal) -> Goal | None:
+        """Finds a composition of goals that refine 'goal_to_refine'.
+
+        MOCK UP OF GREEDY ALGORITHM IN COGOMO, TODO: integrate here
+        """
+
+        for n in range(1, len(self.goals)):
+            for subset in itertools.combinations(self.goals, n):
+                n_compositions = len(subset)
+                if n_compositions == 1:
+                    subset_typeset = subset[0].contract.typeset
+                else:
+                    subset_typeset = reduce(
+                        (lambda x, y: x | y), [g.contract.typeset for g in subset]
+                    )
+                if not self.covers(goal_to_refine, subset_typeset)[0]:
+                    continue
+                g = g_composition(subset)
+                if g.contract <= goal_to_refine.contract:
+                    return g
+
+        return None
+
+    def covers(self, goal: Goal, typeset: Typeset = None) -> (bool, int):
+        """Return true if all the types of the goal are covered by the same
+        type or refinements.
+
+        It also returns the number of similar types
+        """
+        if typeset is None:
+            typeset = self._typeset
+        n_t_covered = 0
+        for t_goal in goal.contract.typeset.values():
+            t_covered = False
+            for t_lib in typeset.values():
+                if t_lib <= t_goal:
+                    t_covered = True
+                    continue
+            if not t_covered:
+                return False, n_t_covered
+            else:
+                n_t_covered += 1
+
+        if n_t_covered == goal.contract.typeset.size:
+            return True, n_t_covered
+        return False, n_t_covered
