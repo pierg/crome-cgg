1cc00608912a12d4cbf8e10bd70db09af17f22a7
Ronzon Mathis <mathis.ronzon@ecole.ensicaen.Fr>
2022-05-23 11:22:34 +0200

    check satisfiable for operations on context

--- crome_cgg/context/__init__.py
+++ crome_cgg/context/__init__.py
@@ -12,22 +12,28 @@ class Context(LTL):
 def group_conjunction(elements: set[Context]) -> Context:
     typeset = Typeset.from_typesets([c.typeset for c in elements])
     formula = and_([c.formula for c in elements])
+    context = Context(_init_formula=formula, _typeset=typeset)
+    if not context.is_satisfiable:
+        raise ContextException(elements)
 
-    return Context(_init_formula=formula, _typeset=typeset)
+    return context
 
 
 def group_disjunction(elements: set[Context]) -> Context:
     typeset = Typeset.from_typesets([c.typeset for c in elements])
     formula = or_([c.formula for c in elements])
+    context = Context(_init_formula=formula, _typeset=typeset)
+    if not context.is_satisfiable:
+        raise ContextException(elements)
 
-    return Context(_init_formula=formula, _typeset=typeset)
+    return context
 
 
 @dataclass(kw_only=True)
 class ContextException(Exception):
 
     def __init__(self, contexts: set[Context]):
-        contexts_str = "\n\t".join(repr(c) for c in contexts)
+        contexts_str = "\n\t".join(c.formula for c in contexts)
         self.message = "*** ContextException EXCEPTION ***\n" + f"A failure has occurred on contexts, their are not " \
                                                            f"compatible:\n\t{contexts_str} "
         super().__init__(self.message)
